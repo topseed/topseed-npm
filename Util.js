5a80ccbf5d4d674594f60cb5d9bdfe05cdf92d1a
puppetmaster3 <vicmasons@gmail.com>
2017-03-28 20:18:44 -0400

    M

--- Util.js
+++ Util.js
@@ -76,14 +76,14 @@ getPath(ROOT, req) {
 	path = this.endsWithSlash(path)
 	return path
 }
-
+/*
 isW(req) { // should we serve SPA or mobile/AMP?
 	if(req.path.startsWith('/w/')) return true
 	if(req.subdomains.indexOf('www') > -1)  return true
 	if(req.socket.localPort == 8082) return true
 	if(req.query.w == '1') return true
 	return false
-}
+}*/
 
 
 replace(target, search, replacement) {
