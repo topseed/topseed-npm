9db49c7de59a8bf89cf27e4f701d80ce2d7e472f
puppetmaster3 <vicmasons@gmail.com>
2017-04-04 07:56:55 -0400

    K

--- 1.08/setup.js
+++ 1.08/setup.js
@@ -59,7 +59,7 @@ function loadIE() { //load fetch, since not in IE
 loadjs([
 	 '//cdn.jsdelivr.net/jquery/3.2.0/jquery.min.js'
 	,'//d2wy8f7a9ursnm.cloudfront.net/bugsnag-3.min.js'
-	,'https://cdn.rawgit.com/topseed/topseed-npm/master/v1/deps/js.cookie.min.js'
+	,'https://cdn.rawgit.com/topseed/topseed-npm/master/v1.08/deps/js.cookie.min.js'
 	], { success: function(){
 		console.log('setup libs loaded!')
 		loadjs.done('keyLibs')
@@ -71,65 +71,3 @@ function preLImg(arg) { // start loading an image so browser has it, for example
 	var imag = new Image()
 	imag.src = arg
 }
-
-var X = { // XHR for SPA
-	clearCookies: function () {
-		var cookies = document.cookie.split(';')
-		for (var i = 0; i < cookies.length; i++){
-			var spcook =  cookies[i].split('=')
-			document.cookie = spcook[0] + '=;expires=Thu, 21 Sep 1979 00:00:01 UTC;'
-		}
-	}//()
-
-	,DT_ : 'DT_' 
-	,writeC: function(nam, str) {
-		var jstr = JSON.stringify(str)
-		var exp = 1/12  // 2 hours
-
-		var hash = window.btoa(jstr)
-		Cookies.set(nam, hash, { expires: exp})
-		Cookies.set(X.DT_, new Date(), { expires: 10 })// so we know when it will time out
-	}//()
-
-	,XBASIC : 'X-BASIC'
-	,XJT : 'X-JWT'
-	,fetch: function(ROOT_, url_, data_) {
-		var xjt_ = Cookies.get(X.XJT)
-		var xb_  = Cookies.get(X.XBASIC)
-		console.log('fetching ', url_, xb_)
-		return fetch(ROOT_ + url_ , { //1 call
-				method: 'post'
-				, headers: {
-					'Content-Type': 'application/json',
-					XJT : xjt_,
-					XBASIC: xb_
-				}
-				, body: JSON.stringify(data_)
-			}).then(function(response) { //2 returns a promise
-				//console.log(response.headers)
-
-				if (!response.ok) {
-					console.log('not ok')
-					console.log(response)
-					throw Error(response.statusText)
-				}
-				return (response.json())//,response.headers.get('X-JWT'))
-			})
-	}//_()
-}
-//> ====================================================================
-/*
-in main.js var ROOT = 'http://localhost:8080/'
-ex fetch use:
-	X.fetch(ROOT, 'RM/signUp', dat)
-		.then(function(value) { 
-			console.log('back')
-			//console.log(JSON.stringify(value))
-			...
-
-	}).catch(function(err) { //error
-		console.error('err')
-		console.error(err)
-	})//fetch()
-*/
-
---
+++ dBindEx/webroot/_tsts/tst1.js
@@ -0,0 +1,31 @@
+
+QUnit.test( 'test: fetch()', function( assert ) {
+
+	loadjs([
+		'/_js/DSrv.js'
+		], { success: function(){
+			console.log('CSrv')
+			var done = assert.async()
+
+			doTest(done)
+		}
+	})
+
+})//tst
+
+function doTest(done) {
+	fetch('https://middle4top-wngjgwtdbn.now.sh/membersPg/mem/', { //1 call
+			method: 'post'
+		}).then(function(response) { //2 return a promise
+			return (response.json())
+			}).then(function(value) { // 3 done:
+				// your code here:
+				console.log('back')
+				console.log(JSON.stringify(value))
+				assert.ok( JSON.stringify(value), 'we got something, check console' )
+				done()
+
+	})//fetch()
+
+}
+
