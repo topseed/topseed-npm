5d7b42f69b00f82eb15b956bd6b5f8cfb570cb01
puppetmaster3 <vicmasons@gmail.com>
2017-04-04 11:06:26 -0400

    D

--- dBindEx/webroot/_tsts/tst1.js
+++ dBindEx/webroot/_tsts/tst1.js
@@ -1,27 +1,31 @@
 
 QUnit.test( 'test: fetch()', function( assert ) {
+	
+	assert.expect(0)
 
 	loadjs([
 		'/_js/DSrv.js'
 		], { success: function(){
 
-			var done = assert.async()
-			doTest(done)
-
+			doTest(assert)
 		}
 	})
 
 })//tst
 
-function doTest(done) {
-	fetch('https://middle4top-wngjgwtdbn.now.sh/membersPg/mem/', { //1 call
-			method: 'post'
+function doTest(assert) {
+	var done = assert.async()
+
+	fetch('http://jsonplaceholder.typicode.com/comments', { //1 call
+			//method: 'post'
 		}).then(function(response) { //2 return a promise
+
 			return (response.json())
 			}).then(function(value) { // 3 done:
 				// your code here:
 				console.log('back')
 				console.log(JSON.stringify(value))
+				
 				assert.ok( JSON.stringify(value), 'we got something, check console' )
 				done()
 
