8881ece72401a3a822850b5aef0dd975404bce5f
puppetmaster3 <vicmasons@gmail.com>
2017-05-13 10:51:35 -0400

    adding rest for cms

--- Util.js
+++ Util.js
@@ -10,82 +10,86 @@ const _slash = '/'
 
 class Util {
 
-getAgent(req) {
-	var agent = useragent.lookup(req.headers['user-agent'])
-	return agent.toAgent()
-}
+	getAgent(req) {
+		var agent = useragent.lookup(req.headers['user-agent'])
+		return agent.toAgent()
+	}
 
-crypt(str, salt) {
-	let hash = crypto.createHmac('sha512', salt)
-	hash.update(str)
-	const value = hash.digest('hex')
-	return value
-}
+	crypt(str, salt) {
+		let hash = crypto.createHmac('sha512', salt)
+		hash.update(str)
+		const value = hash.digest('hex')
+		return value
+	}
 
-genRandom(low, hi){
-	return random.integer(low, hi)
-}
+	genRandom(low, hi){
+		return random.integer(low, hi)
+	}
 
-getDt() {
-	return new Date()
-}
+	getDt() {
+		console.log('deprecated')
+		return new Date()
+	}
 
-getIP(req) {
-	return (req.headers['x-forwarded-for'] || '').split(',')[0] 
-|| req.connection.remoteAddress
-}
+	getIP(req) {
+		return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+	|| req.connection.remoteAddress
+	}
 
-cacheNone(res) {
-	res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
-}
+	cacheNone(res) {
+		res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+	}
 
-cacheQuick(res) {//3hr, 30 seconds
-	res.header('Cache-Control', 'public, s-maxage=10800, max-age=30, proxy-revalidate')
-}
+	cacheQuick(res) {//3hr, 30 seconds
+		res.header('Cache-Control', 'public, s-maxage=10800, max-age=30, proxy-revalidate')
+	}
 
-cacheLong(res) {//23 hours, 2 minutes
-	res.header('Cache-Control', 'public, s-maxage=82800, max-age=120')
-}
+	cacheLong(res) {//23 hours, 2 minutes
+		res.header('Cache-Control', 'public, s-maxage=82800, max-age=120')
+	}
 
-err(msg, res) {
-	console.log('U.err')
-	console.error(msg)
-	res.statusMessage = msg
-	res.status(400).end()
-}
+	err(msg, res) {
+		console.error('uerr', msg)
+		res.statusMessage = msg
+		res.status(400).end()
+	}
 
-/*
-ifError(err, msg, res) {
-	if (err)  {
-		console.log(msg+': ' + err)
-		res.redirect('/index.html')// error - go home
-		res.end()
-		return true
-	} else return false
-}
-*/
-get _slash () {return  '/'}
-endsWithSlash(str ) {
-	if (isj.endWith(str, this._slash)) 
-		return str
-	return str+_slash
-}
-getPath(rot, req) {
-	let path = req.path
-	if (isj.not.existy(path)) path = ''
-	path = rot + req.baseUrl + path
-	//console.log(path)
+	randomString(len) {
+		var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+		var randomString = ''
+		for (var i = 0; i < len; i++) {
+			var randomPoz = Math.floor(Math.random() * charSet.length)
+			randomString += charSet.substring(randomPoz,randomPoz+1)
+		}
+		return randomString
+	}//()
 
-	path = path.replace('undefined/','')
-	path = path.replace('undefined','')
-	path = this.endsWithSlash(path)
-	return path
-}
+	 get ut() {//UTC/gmt time as long 
+		return Date.now()
+	}//()
+
+	get _slash () {return  '/'}
+	endsWithSlash(str ) {
+		if (isj.endWith(str, this._slash)) 
+			return str
+		return str+_slash
+	}
+	getPath(rot, req) {
+		let path = req.path
+		if (isj.not.existy(path)) path = ''
+		path = rot + req.baseUrl + path
+		//console.log(path)
+
+		path = path.replace('undefined/','')
+		path = path.replace('undefined','')
+		path = this.endsWithSlash(path)
+		return path
+	}
 
 
-replace(target, search, replacement) {
-	return target.split(search).join(replacement)
-}//()
+	replace(target, search, replacement) {
+		return target.split(search).join(replacement)
+	}//()
 
 }//class
 module.exports = Util
